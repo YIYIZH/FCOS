1a5f77eb196ad3b0c35b2b7e4dbad9047ad4b5e0
yiyimonica <yiyimonica@gmail.com>
2020-04-20 19:23:53 +0800

    prepare data

--- prepare_data.py
+++ prepare_data.py
@@ -11,24 +11,40 @@ import json
 from shutil import copyfile
 from PIL import Image
 import os
-
+import cv2
+import numpy as np
 import concurrent.futures
 
 width = 1280
 height = 720
 infile = '/dlwsdata3/public/bdd/day_night_256*256/test_latest/images/'
-outfile = '/dlwsdata3/public/bdd/day_night_256*256/test_latest/test1/'
-
+outfile = '/dlwsdata3/public/bdd/day_night_256*256/test_latest/images_resizeto1280/'
 
 def resize_fake(files):
     im = Image.open(infile + files)
     out = im.resize((width, height), Image.ANTIALIAS)
     out.save(outfile + files)
 
+def save_img(image, addr, num):
+    address = addr + str(num) + '.png'
+    cv2.imwrite(address, image)
+
+def vedio():
+    gap = 30
+
+    video = cv2.VideoCapture('demo/demo.avi')
+    success, frame = video.read()
+    count = 0
+    while success:
+        count += 1
+        if (count % gap == 0):
+            save_img(frame, 'output/data1/', count)
+            print('save image', count)
+        success, frame = video.read()
 
 if __name__ == '__main__':
-    dirs = os.listdir(infile)
-    files = [file for file in dirs if 'fake' not in file]
+    dirs = os.listdir(outfile)
+    files = [file for file in dirs if os.path.getsize(outfile + file) == 0]
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         executor.map(resize_fake, files)
